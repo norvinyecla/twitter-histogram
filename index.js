ce58422c60bd1872acf775e2d21a590029e4bde9
Norvin Yecla <ngydev@outlook.com>
2017-04-22 14:38:01 +0800

    Use pug templating engine

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-  res.send('Welcome to Tw-Graph');
+  res.render('index',
+	{ title: 'Welcome to Tw-Graph' } );
 });
 
 app.listen(3000, function() { 
